296d147fb4c2bcb25d59a016c8feb614319bb9bd
gevannmullins <gevann.mullins@gmail.com>
2021-10-14 11:56:25 +0200

    Trying to get the slider working. Testing more code.

--- wp-content/themes/impact/functions.php
+++ wp-content/themes/impact/functions.php
@@ -33,3 +33,53 @@ function post_type_impact_case_studies() {
 }
 add_action('init', 'post_type_impact_case_studies');
 /*Custom Post type end*/
+
+
+// Register a slider block.
+add_action('acf/init', 'my_register_blocks');
+function my_register_blocks() {
+
+    // check function exists.
+    if( function_exists('acf_register_block_type') ) {
+
+        // register a testimonial block.
+        acf_register_block_type(array(
+            'name'              => 'slider',
+            'title'             => __('Slider'),
+            'description'       => __('A custom slider block.'),
+            'render_template'   => 'block-template-parts/slider/slider.php',
+            'category'          => 'formatting',
+            'icon' 				=> 'images-alt2',
+            'align'				=> 'full',
+            'enqueue_assets' 	=> function(){
+                wp_enqueue_style( 'slick', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), '1.8.1' );
+                wp_enqueue_style( 'slick-theme', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css', array(), '1.8.1' );
+                wp_enqueue_script( 'slick', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), '1.8.1', true );
+
+                wp_enqueue_style( 'block-slider', get_template_directory_uri() . '/block-template-parts/slider/slider.css', array(), '1.0.0' );
+                wp_enqueue_script( 'block-slider', get_template_directory_uri() . '/block-template-parts/slider/slider.js', array(), '1.0.0', true );
+            },
+        ));
+    }
+}
+
+
+
+// Register a Images Carousel ACF Block
+if( function_exists('acf_register_block') ) {
+
+    $result = acf_register_block(array(
+        'name'              => 'image_carousel', // Name of our block
+        'title'             => __('Image Carousel'), // Title of our block
+        'description'       => __('A custom Image Carousel block.'), // Description of our block
+        'render_callback'   => 'image_carousel_block_html',// Callback function ( the once that contain the template of our block )
+        'category'          => 'layout',// The category in which the block will be inserted
+        'icon'              => 'format-gallery', // The icon associated with the block ( choose from wordpress dashicons )
+        //'keywords'        => array(),
+    ));
+}
+
+// Callback to render the testimonial ACF Block
+function image_carousel_block_html() {
+
+}
